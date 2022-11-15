b4c9df4f062d2df6a38043700c97a8ea35ad9956
Ericdm094 <eric094@outlook.com>
2022-11-15 14:03:27 -0300

    alteração programs finais

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import os
 os.system('ECHO instalando todos os seus programas da lista.')
-os.system('winget install -e --id Google.Chrome')
-os.system('winget install -e --id VideoLAN.VLC')
+os.system('winget install -e --id OpenJS.NodeJS')
+os.system('winget install -e --id Git.Git')
 os.system('winget install -e --id Microsoft.VisualStudioCode')
 os.system('winget install -e --id Python.Python.3.9')
 os.system('winget install -e --id RARLab.WinRAR')
+#avast v
 os.system('winget install -e --id XPDNZJFNCR1B07')
 os.system('winget install -e --id Discord.Discord')
+os.system('winget install -e --id EpicGames.EpicGamesLauncher')
+os.system('winget install -e --id Valve.Steam')
+os.system('winget install -e --id Spotify.Spotify')
 os.system('ECHO instalando todas as atualizações dos seus programas.')
 os.system('winget upgrade --all')
 os.system('pause')
